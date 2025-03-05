721f5328af434733b9ab7b5a5c240173dc72a9c1
Chris Romp <ChrisRomp@users.noreply.github.com>
2025-03-05 23:18:06 +0000

    Add Azure Virtual Network, Subnet, and Private Endpoint for Storage Account

--- infra/main.tf
+++ infra/main.tf
@@ -29,6 +29,24 @@ data "azurerm_resource_group" "rg" {
     name = var.resource_group_name
 }
 
+# Virtual Network and Subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.storage_account_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.storage_account_name}-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  
+  # Required for private endpoints
+  private_endpoint_network_policies_enabled = true
+}
+
 # storage account
 resource "azurerm_storage_account" "storage" {
     name                          = var.storage_account_name
@@ -38,7 +56,43 @@ resource "azurerm_storage_account" "storage" {
     account_replication_type      = "LRS"
     enable_https_traffic_only     = true
     shared_access_key_enabled     = false
-    # public_network_access_enabled = false
+    public_network_access_enabled = false
+}
+
+# Private DNS Zone for Azure Blob Storage
+resource "azurerm_private_dns_zone" "storage_dns" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Link the Private DNS Zone to the VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "storage_dns_link" {
+  name                  = "${var.storage_account_name}-dns-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
+# Private Endpoint for Storage Account
+resource "azurerm_private_endpoint" "storage_endpoint" {
+  name                = "${var.storage_account_name}-endpoint"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet.id
+
+  private_service_connection {
+    name                           = "${var.storage_account_name}-privateserviceconnection"
+    private_connection_resource_id = azurerm_storage_account.storage.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+
+  # DNS configuration
+  private_dns_zone_group {
+    name                 = "storage-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.storage_dns.id]
+  }
 }
 
 # STATE_RESOURCE_GROUP=xxxx
